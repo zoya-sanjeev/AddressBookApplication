015c38ad752b1cee23a417adb5039daf2dbe736a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-05 23:32:31 +0530

    ADDED: handled exception when address not found

--- src/main/java/com/bridgelabz/addressbook/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbook/exceptions/AddressBookExceptionHandler.java
@@ -16,11 +16,19 @@ import com.bridgelabz.addressbook.dto.ResponseDTO;
 public class AddressBookExceptionHandler {
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ResponseDTO> handleMethodArgumentsNotValidException(MethodArgumentNotValidException exception) {
+	public ResponseEntity<ResponseDTO> handleMethodArgumentsNotValidException(
+			MethodArgumentNotValidException exception) {
 		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
 		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
 		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",errMsg);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(AddressBookException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+			AddressBookException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
 	}
 
 }
