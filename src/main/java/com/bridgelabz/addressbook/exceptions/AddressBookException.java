bbe84a3d47919344536da701e99340949a83e134
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-05 23:27:43 +0530

    ADDED: created custom exception class AddressBookException

---
+++ src/main/java/com/bridgelabz/addressbook/exceptions/AddressBookException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook.exceptions;
+
+public class AddressBookException extends RuntimeException {
+	public AddressBookException(String message) {
+		super(message);
+	}
+
+}
