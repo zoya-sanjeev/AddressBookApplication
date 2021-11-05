da91409a33e394e337a16dbc390623dd40cc6c81
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-05 21:05:09 +0530

    ADDED: created AddressBookService

---
+++ src/main/java/com/bridgelabz/addressbook/services/AddressBookService.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.addressbook.services;
+
+import java.util.List;
+
+import com.bridgelabz.addressbook.dto.AddressBookDTO;
+import com.bridgelabz.addressbook.model.AddressBookData;
+
+public class AddressBookService implements IAddressBookService{
+
+	@Override
+	public List<AddressBookData> getAddressBookData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressBookData getAddressBookDataById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressBookData createAddressBookDaat(AddressBookDTO addressBookDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressBookData updateAddressBopkData(int id, AddressBookDTO addressBookDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAddressBookData(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
