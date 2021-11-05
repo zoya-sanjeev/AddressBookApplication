8629687dad13366f5ed902272371cf365386b685
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-05 22:16:10 +0530

    ADDED: environment variable created in application.properties

--- src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
@@ -2,12 +2,16 @@ package com.bridgelabz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
 	}
 
 }
