9bb7e24fbdbac5176f9a6ac5d4271d3112ffa29c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-08 23:00:15 +0530

    ADDED: injected AddressBookRepository by autowiring

--- src/main/java/com/bridgelabz/addressbook/services/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook/services/AddressBookService.java
@@ -3,15 +3,22 @@ package com.bridgelabz.addressbook.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.addressbook.dto.AddressBookDTO;
 import com.bridgelabz.addressbook.exceptions.AddressBookException;
 import com.bridgelabz.addressbook.model.AddressBookData;
+import com.bridgelabz.addressbook.repository.AddressBookRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class AddressBookService implements IAddressBookService{
 
+	@Autowired
+	private AddressBookRepository addressRepository;
 	private List<AddressBookData> addressBookList = new ArrayList();
 	@Override
 	public List<AddressBookData> getAddressBookData() {
