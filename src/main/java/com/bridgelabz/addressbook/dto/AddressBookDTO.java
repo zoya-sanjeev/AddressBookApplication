fd7d9405006f095580cdd85880595ea1fe3017fa
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-08 21:07:50 +0530

    ADDED: @ToString annotation in AddressBookDTO

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -2,7 +2,9 @@ package com.bridgelabz.addressbook.dto;
 
 import javax.validation.constraints.Pattern;
 
-public class AddressBookDTO {
+import lombok.ToString;
+
+public @ToString class AddressBookDTO {
 	@Pattern(regexp = "^[A-Z|a-z]{2,}$",message ="First Name Invalid")
 	public String firstName;
 	@Pattern(regexp = "^[A-Z|a-z]{2,}$",message ="Last Name Invalid")
@@ -18,21 +20,5 @@ public class AddressBookDTO {
 	@Pattern(regexp = "^[a-zA-Z]{1,}([\\.\\_\\-\\+]?[a-zA-Z0-9]+)@[a-z0-9]+[.][a-z]+[.]?[a-z]{2,3}?$\"",message ="Email invalid")
 	public String email;
 	
-	public AddressBookDTO(String firstName,String lastName, String address,
-		String city, String state, int zip, long phoneNumber,String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-	}
 	
-	@Override
-	public String toString() {
-		return "AddressBook [name=" + firstName +" "+ lastName+ ", address=" + address +  
-				",city="+ city+ ",state"+ state + ",zip="+ zip+", phoneNumber="+ phoneNumber + ", email=" + email + "]";
-	}
 }
--- src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
@@ -2,7 +2,9 @@ package com.bridgelabz.addressbook.model;
 
 import com.bridgelabz.addressbook.dto.AddressBookDTO;
 
-public class AddressBookData {
+import lombok.Data;
+
+public @Data class AddressBookData {
 	private int id;
 	private String firstName;
 	private String lastName;
@@ -12,61 +14,6 @@ public class AddressBookData {
 	private int zip;
 	private long phoneNumber;
 	private String email;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public int getZip() {
-		return zip;
-	}
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(long phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	
 	public AddressBookData(int id, AddressBookDTO addressBookDTO) {
 		this.id = id;
